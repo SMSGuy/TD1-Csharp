0764c3e30d7d8bf38518a516a2f8fb0b1f519fe1
Association SMS <guy_zaegel@hotmail.fr>
2023-09-15 13:44:35 +0200

    Ajoutez des fichiers projet.

---
+++ TD1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //montant Entier;
+            Int32 montant;
+            Console.WriteLine("Saisir le montant à décomposer");
+            montant = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(montant / 100);
+            montant = montant / 100;
+            Console.Writeline(montant);
+            montant = montant / 50;
+            Console.Writeline(montant);
+            montant = montant / 20;
+            Console.Writeline(montant);
+            montant = montant / 10;
+            Console.Writeline(montant);
+            montant = montant / 5;
+            Console.Writeline(montant);
+            Console.ReadKey();
+
+        }
+    }
+}
